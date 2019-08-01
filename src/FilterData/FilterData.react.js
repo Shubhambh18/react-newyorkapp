00cd8c91a6cb5373e3f86a280c8c40683b7cd4bc
shubhambh18 <spoildworld@gmail.com>
2019-08-01 02:50:01 -0700

    filter added

--- src/FilterData/FilterData.react.js
+++ src/FilterData/FilterData.react.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
-
-
-const FilterData = () => {
+const FilterData = (props) => {
 
     return (
-        <div >
-
-        </div>
-    );
+        props.posts.map(
+            p => {
+                if (Date.parse(props.inputsToFilter.to_date) < Date.parse(props.inputsToFilter.from_date)) {
+                    this.variable = 1;
+                    return (
+                        <div>
+                            <h2>Invalid Date</h2>
+                        </div>
+                    )
+                }
+                else if (Date.parse(p.pub_date) >= Date.parse(props.inputsToFilter.from_date) && Date.parse(p.pub_date) <= Date.parse(props.inputsToFilter.to_date)) {
+                    return (
+                        <div key={p.web_url} align='left'>
+                            Abstract: {p.abstract}<br />
+                            News Desk: {p.news_desk}<br />
+                            Section Name:{p.section_name}<br />
+                            Date: {p.pub_date}<br />
+                           
+                            Type:{p.type_of_material}
+                            <hr />
+                        </div>
+                    )
+                }
+                else {
+                    // console.log("date not in range");
+                }
+            }
+        ));
 
 }
 
--- src/Search/Search.react.js
+++ src/Search/Search.react.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import axios from 'axios'
+import FilterData from '../FilterData/FilterData.react';
 
 
 
 class Search extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            posts: [],
-            name:''
-           
+        this.state ={
+        posts :[],        
+        inputsToFilter:{
+        "name":'',
+        "to_date":'',
+        "from_date":'',
+        "ndesk":'',
+        "section":'',
+        "type_of_material":''
+
         }
-    }
-    handleChange=event=>{
-        this.setState({
-            name:event.target.value,
-           
-        })
-    }
+        }
+        }
+        handleChange=(event)=>{
+            let vname=event.target.name;
+            let value =event.target.value;
+            this.setState(prevState=>({inputsToFilter:{...prevState.inputsToFilter ,[vname]: value}}));
+            }
+            
     getPosts = () => {
-        let url="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+this.state.name+"&api-key=jK2AjLdSMvSJJETWeCpL5SyFa5kDulvN"
+        let url="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+(this.state.inputsToFilter.name)+"&fq=news_desk:"+(this.state.inputsToFilter.ndesk)+"&fq=section_name:"+(this.state.inputsToFilter.section)+"&fq=type_of_material:"+(this.state.inputsToFilter.type_of_material)+"&api-key=jK2AjLdSMvSJJETWeCpL5SyFa5kDulvN"
         axios.get(url)
             .then(response => {
                 const data = JSON.parse(JSON.stringify(response.data.response.docs));
@@ -32,27 +40,27 @@ class Search extends React.Component {
 
 
     render() {
-        let postList = this.state.posts.map(
-            value => (
-                <div key={value.abstract}>
-                    Abstract: {value.abstract}<br />
-                    News Desk: {value.news_desk}<br />
-                    Section Name:{value.section_name}<br />
-                    Type:{value.type_of_material}<br />
-                    <hr />
-                </div>
-            )
-        );
+        // let postList = this.state.posts.map(
+        //     value => (
+        //         <div key={value.abstract}>
+        //             Abstract: {value.abstract}<br />
+        //             News Desk: {value.news_desk}<br />
+        //             Section Name:{value.section_name}<br />
+        //             Type:{value.type_of_material}<br />
+        //             <hr />
+        //         </div>
+        //     )
+        // );
         return (
             <div >
                 <div className='row'>
                     <div className='col'>
                         <h4>Search</h4><br/><br/>
-                        <input type="text" value={this.state.name} onChange={this.handleChange}></input>
+                        <input type="text"  name="name" onChange={this.handleChange} placeholder="Enter Article to search"></input>
                         <h6>From</h6>
-                        <input type="date" name="fdate"></input>
+                        <input type="date"  name ="from_date"  onChange={this.handleChange}></input>
                         <h6>To</h6>
-                        <input type="date" name="tdate"></input>
+                        <input type="date" name ="to_date" onChange={this.handleChange}></input>
                     </div>
                     <div className='col-4'>
                         <span>
@@ -60,15 +68,15 @@ class Search extends React.Component {
                         <div className='row'>
                             <span>
                             <h6 className='col'>News Desk</h6>
-                            <input type="text"   ></input>
+                            <input type="text" name="ndesk" onChange={this.handleChange} ></input>
                             </span>
                             <span>
                             <h6 className='col'>Section</h6>
-                            <input type="text"></input>
+                            <input type="text" name="section" onChange={this.handleChange}></input>
                             </span>
                             <span>
                             <h6 className='col'>Type</h6>
-                            <input type="text"></input>
+                            <input type="text" name="type_of_material" onChange={this.handleChange}></input>
                             </span>
                             
                         </div>
@@ -77,8 +85,9 @@ class Search extends React.Component {
                     </div>
                 </div>
                 <div><br/><br/>
-                    <button onClick={this.getPosts}>Search </button>
-                    {postList}
+                    <button onClick={this.getPosts} className='btn btn-primary  btn-lg'> Search </button>
+                    <FilterData posts={this.state.posts} inputsToFilter={this.state.inputsToFilter}></FilterData>
+                     {/* {postList}  */}
                 </div>
             </div>
         );
